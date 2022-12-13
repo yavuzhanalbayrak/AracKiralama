f7c6900295bc31f6a78d9b7e60e4e2ba589620fa
Yavuzhan ALBAYRAK <82886846+yavuzhanalbayrak@users.noreply.github.com>
2022-12-13 13:04:35 +0300

    Araç Kiralama

---
+++ WebProje/WebProje/Controllers/AraclarController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebProje.Controllers
+{
+    public class AraclarController : Controller
+    {
+        public IActionResult Araba()
+        {
+            return View();
+        }
+        public IActionResult Ticari()
+        {
+            return View();
+        }
+        public IActionResult Kamyon()
+        {
+            return View();
+        }
+        public IActionResult Motor()
+        {
+            return View();
+        } public IActionResult Tekne()
+        {
+            return View();
+        }
+    }
+}
