adfb060fdcbf39e795176a81865685095fe95c3c
Yavuzhan ALBAYRAK <82886846+yavuzhanalbayrak@users.noreply.github.com>
2022-12-18 23:49:46 +0300

    Araç Kiralama

--- WebProje/WebProje/Controllers/AraclarController.cs
+++ WebProje/WebProje/Controllers/AraclarController.cs
@@ -10,139 +10,86 @@ namespace WebProje.Controllers
     public class AraclarController : Controller
     {
         AppDbContext _context=new AppDbContext();
-        public IActionResult Araba(int id)
+        public IActionResult Araba(int id, string aracTur)
         {
 
             var cookieValue = Request.Cookies["id"];    //Cookie Çağırma (id) KULLANICI ADI TUTMA
             var kul = _context.kullanici.FirstOrDefault(x => x.id == int.Parse(cookieValue));
-            ViewBag.name = kul.kullaniciAdi;
-
-            var arac = _context.araclar.Where(x => x.tur == "araba" && x.durum == "listede");
-            if (id==2) {    //FİLTRELEME İŞLEMİ
-                arac = _context.araclar.Where(x => x.tur == "araba" && x.durum == "listede").OrderByDescending(x => x.id);//İlk Yüklenen
-            }
-            else if (id==3)
-            {
-                arac = _context.araclar.Where(x => x.tur == "araba" && x.durum == "listede").OrderBy(x => x.yil);//Artan 
-            }
-            else if (id==4)
-            {
-                arac = _context.araclar.Where(x => x.tur == "araba" && x.durum == "listede").OrderByDescending(x => x.yil);//Azalan
-            }
-            else arac = _context.araclar.Where(x => x.tur == "araba" && x.durum == "listede").OrderBy(x => x.id);//Son Yüklenen
+            ViewBag.name = kul.kullaniciAdi;    
+            ValuesController api = new ValuesController();
 
 
-          
-           
-            return View(arac);
+
+            return View(api.Getir(id, aracTur));    //Apiden filtrelenmiş araçlar çekildi.
         }
-        public IActionResult Ticari(int id)
+        public IActionResult Ticari(int id, string aracTur)
         {
 
             var cookieValue = Request.Cookies["id"];    //Cookie Çağırma (id)
             var kul = _context.kullanici.FirstOrDefault(x => x.id == int.Parse(cookieValue));
             ViewBag.name = kul.kullaniciAdi;
 
-            var arac = _context.araclar.Where(x => x.tur == "ticari" && x.durum == "listede");
-            if (id == 2)
-            {
-                arac = _context.araclar.Where(x => x.tur == "ticari" && x.durum == "listede").OrderByDescending(x => x.id);//İlk Yüklenen
-            }
-            else if (id == 3)
-            {
-                arac = _context.araclar.Where(x => x.tur == "ticari" && x.durum == "listede").OrderBy(x => x.yil);//Artan 
-            }
-            else if (id == 4)
-            {
-                arac = _context.araclar.Where(x => x.tur == "ticari" && x.durum == "listede").OrderByDescending(x => x.yil);//Azalan
-            }
-            else arac = _context.araclar.Where(x => x.tur == "ticari" && x.durum == "listede").OrderBy(x => x.id);//Son Yüklenen
+            ValuesController api = new ValuesController();
+
+
+
+            return View(api.Getir(id, aracTur));    //Apiden filtrelenmiş araçlar çekildi.
+        }
+        public IActionResult Kamyon(int id, string aracTur)
+        {
+            var cookieValue = Request.Cookies["id"];    //Cookie Çağırma (id)
+            var kul = _context.kullanici.FirstOrDefault(x => x.id == int.Parse(cookieValue));
+            ViewBag.name = kul.kullaniciAdi;
+
             
-            return View(arac);
+
+            ValuesController api = new ValuesController();
+            
+
+
+            return View(api.Getir(id, aracTur));    //Apiden filtrelenmiş araçlar çekildi.
         }
-        public IActionResult Kamyon(int id)
+        public IActionResult Motor(int id, string aracTur)
         {
             var cookieValue = Request.Cookies["id"];    //Cookie Çağırma (id)
             var kul = _context.kullanici.FirstOrDefault(x => x.id == int.Parse(cookieValue));
             ViewBag.name = kul.kullaniciAdi;
 
-            var arac = _context.araclar.Where(x => x.tur == "kamyon" && x.durum == "listede");
-            if (id == 2)
-            {
-                arac = _context.araclar.Where(x => x.tur == "kamyon" && x.durum == "listede").OrderByDescending(x => x.id);//İlk Yüklenen
-            }
-            else if (id == 3)
-            {
-                arac = _context.araclar.Where(x => x.tur == "kamyon" && x.durum == "listede").OrderBy(x => x.yil);//Artan 
-            }
-            else if (id == 4)
-            {
-                arac = _context.araclar.Where(x => x.tur == "kamyon" && x.durum == "listede").OrderByDescending(x => x.yil);//Azalan
-            }
-            else arac = _context.araclar.Where(x => x.tur == "kamyon" && x.durum == "listede").OrderBy(x => x.id);//Son Yüklenen
+            ValuesController api = new ValuesController();
 
-            return View(arac);
-        }
-        public IActionResult Motor(int id)
-        {
-            var cookieValue = Request.Cookies["id"];    //Cookie Çağırma (id)
-            var kul = _context.kullanici.FirstOrDefault(x => x.id == int.Parse(cookieValue));
-            ViewBag.name = kul.kullaniciAdi;
 
-            var arac = _context.araclar.Where(x => x.tur == "motor" && x.durum == "listede");
-            if (id == 2)
-            {
-                arac = _context.araclar.Where(x => x.tur == "motor" && x.durum == "listede").OrderByDescending(x => x.id);//İlk Yüklenen
-            }
-            else if (id == 3)
-            {
-                arac = _context.araclar.Where(x => x.tur == "motor" && x.durum == "listede").OrderBy(x => x.yil);//Artan 
-            }
-            else if (id == 4)
-            {
-                arac = _context.araclar.Where(x => x.tur == "motor" && x.durum == "listede").OrderByDescending(x => x.yil);//Azalan
-            }
-            else arac = _context.araclar.Where(x => x.tur == "motor" && x.durum == "listede").OrderBy(x => x.id);//Son Yüklenen
-            return View(arac);
+
+            return View(api.Getir(id, aracTur));    //Apiden filtrelenmiş araçlar çekildi.
         } 
-        public IActionResult Tekne(int id)
+        public IActionResult Tekne(int id, string aracTur)
         {
             var cookieValue = Request.Cookies["id"];    //Cookie Çağırma (id)
             var kul = _context.kullanici.FirstOrDefault(x => x.id == int.Parse(cookieValue));
             ViewBag.name = kul.kullaniciAdi;
 
-            var arac = _context.araclar.Where(x => x.tur == "tekne" && x.durum == "listede");
-            if (id == 2)
-            {
-                arac = _context.araclar.Where(x => x.tur == "tekne" && x.durum == "listede").OrderByDescending(x => x.id);//İlk Yüklenen
-            }
-            else if (id == 3)
-            {
-                arac = _context.araclar.Where(x => x.tur == "tekne" && x.durum == "listede").OrderBy(x => x.yil);//Artan 
-            }
-            else if (id == 4)
-            {
-                arac = _context.araclar.Where(x => x.tur == "tekne" && x.durum == "listede").OrderByDescending(x => x.yil);//Azalan
-            }
-            else arac = _context.araclar.Where(x => x.tur == "tekne" && x.durum == "listede").OrderBy(x => x.id);//Son Yüklenen
-            return View(arac);
+
+            ValuesController api = new ValuesController();
+
+
+
+            return View(api.Getir(id, aracTur));    //Apiden filtrelenmiş araçlar çekildi.
         }
         
         public IActionResult Detay(int? id, Kiralama kira)
         {
 
                 int kullaniciId = int.Parse(Request.Cookies["id"]);
-                var kullanici=_context.kullanici.FirstOrDefault(x => x.id == kullaniciId); 
-                var arac=_context.araclar.FirstOrDefault(x => x.id == id);
+                var kullanici=_context.kullanici.FirstOrDefault(x => x.id == kullaniciId);  //kullanıcı id'si alındı
+                var arac=_context.araclar.FirstOrDefault(x => x.id == id);  //araç id'si alındı
                 arac.durum = "kiralandi";
                 arac.sahibi = kullanici.kullaniciAdi;
-                _context.araclar.Update(arac);
+                _context.araclar.Update(arac);  //araç kiralandı olarak değiştirildi
                 kira.id = null;
                 kira.aracAdi = arac.marka;
                 kira.aracModeli = arac.model;
                 kira.aracId = id;
                 kira.kullaniciId = int.Parse(Request.Cookies["id"]);
-                _context.Add(kira);
+                _context.Add(kira); //araç ve kullanıcı id'siyle sipariş oluşturuldu.
                 _context.SaveChanges();
 
             TempData["iade"] = arac.id + " id'li " + arac.marka + " " + arac.model + " Kiralandı.";
@@ -151,25 +98,24 @@ namespace WebProje.Controllers
 
            
         }
-        public IActionResult KiralananAraclar()
+        public IActionResult KiralananAraclar()     //Kullanıcıya ait kiralanan araçlar getirildi.
         {
             int kullanici=int.Parse(Request.Cookies["id"]);
             var kul=_context.kullanici.FirstOrDefault(x=>x.id==kullanici);
-            ViewBag.kul = kul.kullaniciAdi;
             var kira = _context.kiralama.Where(x => x.kullaniciId == kullanici);
             return View(kira);
         }
-        public IActionResult Iade(int id)
+        public IActionResult Iade(int id)   //Araç iade edildi.
         {
             var kira = _context.kiralama.FirstOrDefault(x => x.id == id);
             var arac = _context.araclar.FirstOrDefault(x=>x.id==kira.aracId);
 
             arac.sahibi = null;
             arac.durum = "listede";
-            _context.araclar.Update(arac);
+            _context.araclar.Update(arac);  //araç listeye yeniden eklendi.
 
 
-            _context.kiralama.Remove(kira);
+            _context.kiralama.Remove(kira); //Sipariş silme
             _context.SaveChanges();
 
             TempData["iade"] = arac.id + " id'li " + arac.marka+" " + arac.model + " iade edildi.";
---
+++ WebProje/WebProje/Controllers/ValuesController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using WebProje.DBAccess;
+using WebProje.Entities;
+
+namespace WebProje.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        [HttpGet]   
+        public System.Linq.IQueryable Getir(int id , string aracTur)//Filtreleme işlemi yapmaya yarayan api
+        {
+            AppDbContext _context=new AppDbContext();
+            System.Linq.IQueryable arac;
+            if (id == 2)
+            {
+                arac = _context.araclar.Where(x => x.tur == aracTur && x.durum == "listede").OrderByDescending(x => x.id);//İlk Yüklenen
+            }
+            else if (id == 3)
+            {
+                arac = _context.araclar.Where(x => x.tur == aracTur && x.durum == "listede").OrderBy(x => x.yil);//Artan 
+            }
+            else if (id == 4)
+            {
+                arac = _context.araclar.Where(x => x.tur == aracTur && x.durum == "listede").OrderByDescending(x => x.yil);//Azalan
+            }
+            else arac = _context.araclar.Where(x => x.tur == aracTur && x.durum == "listede").OrderBy(x => x.id);//Son Yüklenen
+
+            return arac;    //araç filtrelenmiş şekilde döndürülür.
+        }
+    }
+}
